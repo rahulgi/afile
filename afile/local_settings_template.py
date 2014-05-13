0bd09c4f2d73ec40a9cc259b37130e1cf17b4ee3
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-05-13 01:02:39 -0700

    Initial commit.

---
+++ afile/local_settings_template.py
@@ -0,0 +1,13 @@
+# /fluxy/local_settings_commented.py
+# THESE SETTINGS ARE FOR THE DEV ENVIRONMENT
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = TEMPLATE_DEBUG = True
+
+# People who get notified when errors happen
+ADMINS = ()
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
